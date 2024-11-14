2f7abc62e396476122a7cd4b4609dba8d6a889d6
Pavlo-kik <pavhunskysuslo@gmail.com>
2024-11-14 15:09:54 +0200

    Add files via upload

---
+++ lab_4.1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int N;
+    double result = 1.0;
+
+    cout << "N = "; cin >> N;
+
+    // while цикл
+    int i = N;
+    while (i <= 16) {
+        result *= i * N / (pow(i, 2) + pow(N, 2));
+        i++;
+    }
+    cout << "While: " << result << endl;
+
+    // do-while цикл
+    i = N;
+    do {
+        result *= i * N / (pow(i, 2) + pow(N, 2));
+        i++;
+    } while (i <= 16);
+    cout << "do-while: " << result << endl;
+
+    // do-while цикл
+    i = N;
+    do {
+        result *= i * N / (pow(i, 2) + pow(N, 2));
+        i++;
+    } while (i <= 16);
+    cout << "do-while: " << result << endl;
+
+    // for цикл (--)
+    result = 1.0;
+    for (i = 16; i >= N; i--) {
+        result *= i * N / (pow(i, 2) + pow(N, 2));
+    }
+    cout << "For (--): " << result << endl;
+
+    return 0;
+}
